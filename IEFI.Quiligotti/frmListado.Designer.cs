386567abb6a5818f1b4dccf8ee2a26e022fd9a8d
Alumnos <Alumnos@PC184>
2023-06-30 12:04:49 -0300

    30

--- IEFI.Quiligotti/frmListado.Designer.cs
+++ IEFI.Quiligotti/frmListado.Designer.cs
@@ -54,10 +54,9 @@
             // lblListadoTitulo
             // 
             this.lblListadoTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblListadoTitulo.Location = new System.Drawing.Point(88, 51);
-            this.lblListadoTitulo.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblListadoTitulo.Location = new System.Drawing.Point(44, 27);
             this.lblListadoTitulo.Name = "lblListadoTitulo";
-            this.lblListadoTitulo.Size = new System.Drawing.Size(282, 65);
+            this.lblListadoTitulo.Size = new System.Drawing.Size(141, 34);
             this.lblListadoTitulo.TabIndex = 2;
             this.lblListadoTitulo.Text = "Listado";
             // 
@@ -68,11 +67,12 @@
             this.columnFecha,
             this.ColumnNombre,
             this.ColumnCantidad});
-            this.dtvMatrizProd.Location = new System.Drawing.Point(26, 306);
+            this.dtvMatrizProd.Location = new System.Drawing.Point(13, 159);
+            this.dtvMatrizProd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtvMatrizProd.Name = "dtvMatrizProd";
             this.dtvMatrizProd.RowHeadersWidth = 82;
             this.dtvMatrizProd.RowTemplate.Height = 33;
-            this.dtvMatrizProd.Size = new System.Drawing.Size(736, 589);
+            this.dtvMatrizProd.Size = new System.Drawing.Size(368, 306);
             this.dtvMatrizProd.TabIndex = 3;
             this.dtvMatrizProd.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtvMatrizProd_CellContentClick);
             // 
@@ -100,9 +100,10 @@
             // btnCerrar
             // 
             this.btnCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCerrar.Location = new System.Drawing.Point(1303, 987);
+            this.btnCerrar.Location = new System.Drawing.Point(652, 513);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(212, 57);
+            this.btnCerrar.Size = new System.Drawing.Size(106, 30);
             this.btnCerrar.TabIndex = 6;
             this.btnCerrar.Text = "Cerrar";
             this.btnCerrar.UseVisualStyleBackColor = true;
@@ -111,9 +112,10 @@
             // btnMostrar
             // 
             this.btnMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMostrar.Location = new System.Drawing.Point(195, 987);
+            this.btnMostrar.Location = new System.Drawing.Point(98, 513);
+            this.btnMostrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnMostrar.Name = "btnMostrar";
-            this.btnMostrar.Size = new System.Drawing.Size(339, 57);
+            this.btnMostrar.Size = new System.Drawing.Size(170, 30);
             this.btnMostrar.TabIndex = 7;
             this.btnMostrar.Text = "Mostrar Productos";
             this.btnMostrar.UseVisualStyleBackColor = true;
@@ -126,11 +128,12 @@
             this.columnFechaV,
             this.ColumnProdV,
             this.ColumnCantV});
-            this.dtvMatrizVenta.Location = new System.Drawing.Point(840, 306);
+            this.dtvMatrizVenta.Location = new System.Drawing.Point(420, 159);
+            this.dtvMatrizVenta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtvMatrizVenta.Name = "dtvMatrizVenta";
             this.dtvMatrizVenta.RowHeadersWidth = 82;
             this.dtvMatrizVenta.RowTemplate.Height = 33;
-            this.dtvMatrizVenta.Size = new System.Drawing.Size(812, 589);
+            this.dtvMatrizVenta.Size = new System.Drawing.Size(406, 306);
             this.dtvMatrizVenta.TabIndex = 8;
             this.dtvMatrizVenta.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtvMatrizVenta_CellContentClick);
             // 
@@ -158,9 +161,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(868, 987);
+            this.button1.Location = new System.Drawing.Point(434, 513);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(275, 57);
+            this.button1.Size = new System.Drawing.Size(138, 30);
             this.button1.TabIndex = 9;
             this.button1.Text = "Mostrar Ventas";
             this.button1.UseVisualStyleBackColor = true;
@@ -169,29 +173,28 @@
             // lblListVenta
             // 
             this.lblListVenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblListVenta.Location = new System.Drawing.Point(994, 219);
-            this.lblListVenta.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblListVenta.Location = new System.Drawing.Point(497, 114);
             this.lblListVenta.Name = "lblListVenta";
-            this.lblListVenta.Size = new System.Drawing.Size(282, 65);
+            this.lblListVenta.Size = new System.Drawing.Size(141, 34);
             this.lblListVenta.TabIndex = 10;
             this.lblListVenta.Text = "Ventas";
             // 
             // lblProd
             // 
             this.lblProd.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProd.Location = new System.Drawing.Point(207, 219);
-            this.lblProd.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblProd.Location = new System.Drawing.Point(104, 114);
             this.lblProd.Name = "lblProd";
-            this.lblProd.Size = new System.Drawing.Size(349, 65);
+            this.lblProd.Size = new System.Drawing.Size(174, 34);
             this.lblProd.TabIndex = 11;
             this.lblProd.Text = "Productos";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::IEFI.Quiligotti.Properties.Resources.listado;
-            this.pictureBox1.Location = new System.Drawing.Point(415, 27);
+            this.pictureBox1.Location = new System.Drawing.Point(208, 14);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 155);
+            this.pictureBox1.Size = new System.Drawing.Size(100, 81);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
@@ -200,27 +203,29 @@
             // 
             this.lblSelecc.AutoSize = true;
             this.lblSelecc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSelecc.Location = new System.Drawing.Point(749, 109);
+            this.lblSelecc.Location = new System.Drawing.Point(374, 57);
+            this.lblSelecc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSelecc.Name = "lblSelecc";
-            this.lblSelecc.Size = new System.Drawing.Size(300, 31);
+            this.lblSelecc.Size = new System.Drawing.Size(158, 17);
             this.lblSelecc.TabIndex = 13;
             this.lblSelecc.Text = "Seleccione un Producto";
             // 
             // lstProducto
             // 
             this.lstProducto.FormattingEnabled = true;
-            this.lstProducto.Location = new System.Drawing.Point(1088, 112);
+            this.lstProducto.Location = new System.Drawing.Point(544, 58);
+            this.lstProducto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lstProducto.Name = "lstProducto";
-            this.lstProducto.Size = new System.Drawing.Size(293, 33);
+            this.lstProducto.Size = new System.Drawing.Size(148, 21);
             this.lstProducto.TabIndex = 16;
             this.lstProducto.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // frmListado
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.ClientSize = new System.Drawing.Size(1676, 1119);
+            this.ClientSize = new System.Drawing.Size(836, 581);
             this.Controls.Add(this.lstProducto);
             this.Controls.Add(this.lblSelecc);
             this.Controls.Add(this.pictureBox1);
@@ -233,7 +238,8 @@
             this.Controls.Add(this.dtvMatrizProd);
             this.Controls.Add(this.lblListadoTitulo);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmListado";
             this.Text = "Listado";
             this.Load += new System.EventHandler(this.frmListado_Load);
